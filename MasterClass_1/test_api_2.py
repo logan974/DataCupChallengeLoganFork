6887d1991a26e0e0d0a2add84c261e87b2c8e92f
christopher de Boisvilliers <christopherdeboisvilliers@MacBook-Air-de-christopher.local>
2024-10-06 10:16:53 +0400

    fix unit test issues

--- MasterClass_1/test_api_2.py
+++ MasterClass_1/test_api_2.py
@@ -7,7 +7,7 @@ id_titles = None
 # Classe de tests pour vérifier les résultats de l'API articles
 class TestArticlesAPI(unittest.TestCase):
     def test_id_list(self):
-        expected_id_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        expected_id_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12]
         # Comparer les deux listes sans tenir compte de l'ordre
         self.assertCountEqual(id_list, expected_id_list, "La liste des IDs n'est pas correcte")
 
@@ -22,7 +22,9 @@ class TestArticlesAPI(unittest.TestCase):
             'Faire ses courses en Batmobile',
             "L'art du camouflage en forêt",
             'Les vertus du miel magique',
-            'Pourquoi les aliens adorent les vélos'
+            'Pourquoi les aliens adorent les vélos',
+            'Article de la masterclass'
+
         ]
         # Comparer les deux listes sans tenir compte de l'ordre
         self.assertCountEqual(id_titles, expected_id_titles, "La liste des titres n'est pas correcte")
